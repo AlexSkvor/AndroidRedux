f413331195728bdf57bd81af07b0f5c46cb6a030
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2020-09-29 16:01:00 +0200

    Create machine

---
+++ redux/src/main/java/voodoo/rocks/redux/livedata/LiveDataWithDefault.kt
@@ -0,0 +1,11 @@
+package voodoo.rocks.redux.livedata
+
+import androidx.lifecycle.LiveData
+
+open class LiveDataWithDefault<T: Any>(private val default: T) : LiveData<T>(default) {
+
+    override fun getValue(): T {
+        return super.getValue() ?: default
+    }
+
+}
---
+++ redux/src/main/java/voodoo/rocks/redux/livedata/MutableLiveDataWithDefault.kt
@@ -0,0 +1,12 @@
+package voodoo.rocks.redux.livedata
+
+class MutableLiveDataWithDefault<T: Any>(default: T) : LiveDataWithDefault<T>(default) {
+
+    public override fun postValue(value: T) {
+        super.postValue(value)
+    }
+
+    public override fun setValue(value: T) {
+        super.setValue(value)
+    }
+}
