7f9c1981b7ecd771e5cd78fba80de34054e0cf62
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2020-09-29 16:34:19 +0200

    Add module dependecy

--- app/src/main/java/voodoo/rocks/androidfluxsample/MainActivity.kt
+++ app/src/main/java/voodoo/rocks/androidfluxsample/MainActivity.kt
@@ -5,6 +5,13 @@ import android.os.Bundle
 import voodoo.rocks.flux.interfaces.BackPressedHolder
 
 class MainActivity : AppCompatActivity() {
+
+    private val currentFragment: BackPressedHolder? = null // TODO
+
+    override fun onBackPressed() {
+        currentFragment?.onBackPressed() ?: super.onBackPressed()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
